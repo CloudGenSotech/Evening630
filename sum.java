c17475f781643d5e74e8fa409a6b6c4fc4c18942
ThotaThanish <sriurr@gmail.com>
2018-03-29 09:52:07 +0530

    Update sum.java

--- sum.java
+++ sum.java
@@ -8,6 +8,18 @@ b=100;
 c=500;
 c=a+b+c;
 System.out.println("The sum of three values are "+c);
+
+
+int rate,price,omi,toti,totp,time;
+  
+  rate=2;
+  price=5000;
+  omi=price*rate/100;
+  System.out.println ("The one month intrest is "+omi);
+  time=34;
+  toti=omi*time;
+  System.out.println("The Total Intrest is +"toti);
+  
 }
 }
 
